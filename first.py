63f0f687f427f6ecc2164541733dad717b46b1e5
kartikeya jha <kartikeya.jha@nexturn.com>
2024-10-14 00:32:36 +0530

    Pushing the first code

---
+++ first.py
@@ -0,0 +1,4 @@
+print('this is the first versio of the project')
+print('adding this change after the first git commit')
+print('testing git diff command')
+print('adding new line when a file is already in the staging')
